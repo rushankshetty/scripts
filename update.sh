0bc22fe3c1d09044a128d8f5a5a093a1f2b1a568
rushankshetty <shettyrushank@gmail.com>
2015-04-28 16:25:57 +0530

    Added comments to update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# A quick and dirty script to automate the process of
+# updating the packages on Ubuntu Linux and other debian
+# based operating systems
+
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
